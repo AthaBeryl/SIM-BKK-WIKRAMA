a937acec18cf3a3752a27938171ddb3c59062ca6
Atha Beryl <berylprimapes@gmail.com>
2020-04-09 19:09:20 +0700

    repush for missing directory

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -20,10 +20,12 @@ class ResumeController extends Controller
 
     public function download($userId){
         $alumni = datasiswa::where('user_id',$userId)->first();
+        $status = datastatus::where('user_id',$userId)->latest()->first();
         $kerja = datastatus::whereIn('status',['Kerja','Wirausaha'])->where('nis',$alumni->nis)->orderby('created_at','asc')->get();
         $kuliah = datastatus::where('nis',$alumni->nis)->where('status','Kuliah')->orderby('created_at','asc')->get();
         $user = User::where('id',$userId)->first();
         $pdf = PDF::loadView('report/cv',compact('alumni','user','kerja','kuliah'));
-        return $pdf->download($alumni->name."'s CV.pdf");
+        // return $pdf->download($alumni->name."'s CV.pdf");
+        return view('report.cv',compact('alumni','status','user','kerja','kuliah','status'));
     }
 }
