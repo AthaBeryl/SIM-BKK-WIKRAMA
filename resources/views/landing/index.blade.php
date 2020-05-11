ae0cf9a72e80840900f3d477d2265674f547b80a
NandaryaniDamastutik <muhammadyaumil1212@gmail.com>
2020-05-12 02:20:28 +0700

    update sedikit

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -212,14 +212,6 @@
       <center><i class="far fa-sad-tear"></i><h4>Belum ada Pesan</h4></center>
       @endforelse
     </div>
-    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
     <br>
   </div>
 </div>
